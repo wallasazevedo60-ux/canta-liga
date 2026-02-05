078813d313ac9dc4784a7b875cc8aedc7b45cd92
wallasazevedo60-ux <wallasazevedo60@gmail.com>
2026-02-05 02:33:10 -0300

    Cria AuthPage para corrigir build Vite

---
+++ client/src/pages/auth-page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AuthPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Página de Login</h1>
+      <p>Autenticação vai aqui</p>
+    </div>
+  );
+};
+
+export default AuthPage;
